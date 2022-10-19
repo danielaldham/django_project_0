f621708b60641402fde0c82125e685acbbab5a36
Danny Aldham <daniel.aldham@gmail.com>
2022-10-19 22:48:59 +0200

    real python projects tutorial complete and working

--- cs50_final/cs50_final/settings.py
+++ cs50_final/cs50_final/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from re import template
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'cs50_final.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['cs50_final/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
